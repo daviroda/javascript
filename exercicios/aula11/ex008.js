7049ff69c2ee1049e910d04f31783fe14ebdac82
daviroda <daviroda48@gmail.com>
2024-07-08 16:58:12 -0300

    atualização
    Aprendendo condições no JavaScript (if e else)

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { // condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log(`Digira sempre usando cinto de segurança!`)
