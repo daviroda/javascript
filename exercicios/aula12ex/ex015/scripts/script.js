8ccbb45524c8411a6052bd8b3a8659a65d123e1d
daviroda <daviroda48@gmail.com>
2024-07-17 16:18:02 -0300

    atualização

--- exercicios/aula12ex/ex015/scripts/script.js
+++ exercicios/aula12ex/ex015/scripts/script.js
@@ -7,6 +7,14 @@ function verificar() {
     if (fano.value.length == 0 || fano.value > ano) {
         window.alert('[ERRO]Verifique os dados e tente novamente!')
     } else {
-        
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        if (fsex[0].checked) {
+            genero = 'Homem'
+        } else {
+            genero = 'Mulher'
+        }
     }
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
 }
