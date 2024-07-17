8822b14f509a0648541107c0f5b459d9934d65a9
daviroda <daviroda48@gmail.com>
2024-07-17 13:02:01 -0300

    atualização

--- exercicios/aula12ex/ex015/scripts/script.js
+++ exercicios/aula12ex/ex015/scripts/script.js
@@ -1,1 +1,12 @@
-window.alert('Olá')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO]Verifique os dados e tente novamente!')
+    } else {
+        
+    }
+}
