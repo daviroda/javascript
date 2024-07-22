61c2691be8d20ef343efdf3e6dee255b4591f5a4
daviroda <daviroda48@gmail.com>
2024-07-22 08:04:04 -0300

    atualização
    repetiçõess em JS

---
+++ exercicios/aula13/repetições.js
@@ -0,0 +1,23 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 8) //mais especifica, nesta situação tanto faz essa ou a de baixo
+
+
+/*
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/ //mais encontrada, recomendada
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/ //não indicada
