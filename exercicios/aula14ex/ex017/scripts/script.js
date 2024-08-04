42f77144c74c4e335a52212601f535d95a2437f0
daviroda <daviroda48@gmail.com>
2024-08-04 15:11:38 -0300

    atualização
    pratica JS

--- exercicios/aula14ex/ex017/scripts/script.js
+++ exercicios/aula14ex/ex017/scripts/script.js
@@ -6,6 +6,15 @@ function tabuada() {
         window.alert("Por favor Digite um numero")
     } else {
         let n = Number(num.value) 
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            tab.value = `tab${c}`
+            c++
+        }
     }
     
 }
