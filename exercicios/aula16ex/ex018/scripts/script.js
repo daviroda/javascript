eba5e8172250a48ca415d9757766be6f83938d81
daviroda <daviroda48@gmail.com>
2024-08-20 09:01:09 -0300

    finalização exercícios
    Finalização exercícios - Curso básico JavaScript

--- exercicios/aula16ex/ex018/scripts/script.js
+++ exercicios/aula16ex/ex018/scripts/script.js
@@ -21,8 +21,42 @@ function inLista(n, l) {
 
 function adicionar() {
     if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement("option")
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
        //tudo OK
     } else {
         window.alert("Valor inválido ou já encontrado na lista.")
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0 ){
+        window.alert("Adicione valores antes de finalizar!")
+    } else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0 
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            } else if(valores[pos] < menor) {
+                menor = valores[pos]
+            } //teste para verificar numero maior e numero menor
+        }
+        media = soma / total
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
     }
 }
